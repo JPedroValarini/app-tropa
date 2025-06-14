69c2e9fab151f084740ae4dde66e19ab7ee07142
JPedroValarini <joaopedrovalarini@gmail.com>
2025-06-13 21:23:03 -0300

    vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   },
+  base: '/',
 });
